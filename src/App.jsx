1031f95370463e7ac52d74656c52d22a4115c1e8
Jeffrey Igala <jeffreyigala@MACBOOKPRO.station>
2025-12-03 23:12:02 +0000

    card functionality implementation started

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,62 @@
 
 import { GamerHeader } from "./components/GameHeader";
+import { Card } from "./components/Card";
+import { useEffect } from "react";
+
+
+const cardValues = [
+  "🐶",
+  "🐱",
+  "🐰",
+  "🐻",
+  "🐭",
+  "🐹",
+  "🦊",
+  "🦁",
+  "🐶",
+  "🐱",
+  "🐰",
+  "🐻",
+  "🐭",
+  "🐹",
+  "🦊",
+  "🦁",
+];
 
 function App() {
+  // keeps track of the cards in the game
+  const [cards, setCards] = useState([])
+
+  // keeps track of what happens when the game starts
+  const initializeGame = () => {
+    
+    // loop through the card values and create a card object for each value
+    const finalCards =cardValues.map((value, index) =>(
+      {
+        id: index,
+        value,
+        isFlipped: false,
+        isMatched: false,
+      }))
+
+      // called the user open the game with the new set of cards
+      setCards(finalCards)
+  };
+
+  // 
+  useEffect(() => {
+    initializeGame()
+  }, []);
+
   return  (
   <div className="app">
     <GamerHeader score={3} moves={10}/>
+
+    <div className="cards-grid">
+      {cardValues.map((card) => (
+        <Card card={card} />
+      ))}
+    </div>
     </div>
   );
 }
